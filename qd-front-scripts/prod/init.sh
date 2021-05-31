68c6ae3119b3f09defb68c665042cf5195c43216
DaQuirm <leo.daquirm@gmail.com>
2021-05-31 22:00:57 +0200

    Add auto-renewal

--- qd-front-scripts/prod/init.sh
+++ qd-front-scripts/prod/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-# refresh certificates
 # https://certbot.eff.org/docs/man/certbot.html
 certbot certonly --nginx -d 2123.io -m info@2123.io --agree-tos
+
+# auto-renew certificates
+SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | tee -a /etc/crontabs/root > /dev/null
