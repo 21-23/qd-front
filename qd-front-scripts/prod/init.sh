cf1b3b019806c0b4b6ca1a2a841bd2613bdd40e9
Kiril Knysh <k.knysh@gmail.com>
2021-08-22 14:32:06 +0200

    Use /ws instead of a port;

--- qd-front-scripts/prod/init.sh
+++ qd-front-scripts/prod/init.sh
@@ -5,7 +5,7 @@ crond -L /var/log/crond.log
 
 # https://certbot.eff.org/docs/man/certbot.html
 #certbot certonly --nginx -d 2123.io -m info@2123.io --agree-tos
-certbot --nginx -d 2123.io -d www.2123.io -m info@2123.io --agree-tos --test-cert
+certbot --nginx -d 2123.io -d www.2123.io -m info@2123.io --agree-tos --non-interactive --test-cert
 
 # auto-renew certificates
 SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); echo "0 0,12 * * * sleep $SLEEPTIME && certbot renew -q" | tee -a /etc/crontabs/root > /dev/null
