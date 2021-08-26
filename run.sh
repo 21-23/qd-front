5ed32135ada445647cab42737adbb704fe9efb2d
Kiril Knysh <k.knysh@gmail.com>
2021-08-26 21:06:53 +0200

    Remove unnecessary WS code pieces;

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo "Remove previous container"
 docker rm qd_front 2>/dev/null
 
 echo "Start qd_front"
-docker run -d --name qd_front -p 80:80 -p 443:443 -p 3001:3001 --mount type=bind,source="$(pwd)"/static/2123,destination=/usr/share/nginx/2123 qd_front
+docker run -d --name qd_front -p 80:80 -p 443:443 --mount type=bind,source="$(pwd)"/static/2123,destination=/usr/share/nginx/2123 qd_front
 
 echo "Wait for container"
 sleep 5
